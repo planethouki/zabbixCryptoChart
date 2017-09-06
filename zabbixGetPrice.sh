0a3b8955f1f89b900cefd5c7b2a2d12a2e4e8dc0
planethouki <syn_tok@hotmail.co.jp>
2017-09-06 20:28:45 +0900

    zabbix item key

---
+++ zabbixGetPrice.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ `echo $1 | grep 'null'` ] ; then
+  echo null;
+  exit
+fi
+
+
+case "$1" in
+"coincheck" )
+  curl -sS https://coincheck.com/api/rate/$1 | jq '.rate | tonumber'
+;;
+"bitbank" )
+  curl -X GET --header 'Accept: application/json' 'https://public.bitbank.cc/$1/ticker' | jq '.data.last | tonumber'
+;;
+"poloniex" )
+  curl -sS https://poloniex.com/public?command=returnTicker | jq --arg res $2 '.$1.last | tonumber'
+;;
+"zaif" )
+  curl -Ss https://api.zaif.jp/api/1/ticker/$2 | jq .last
+;;
+"bittrex" )
+  curl -sS https://bittrex.com/api/v1.1/public/getticker?market=$2 | jq '.result.Last'
+;;
+esac
+
+~
+~
