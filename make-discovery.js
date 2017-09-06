25382e63aa2fc270242cb4a64e66c6bb694ef16d
planethouki <syn_tok@hotmail.co.jp>
2017-09-06 16:21:27 +0900

    write

--- make-discovery.js
+++ make-discovery.js
@@ -1,45 +1,58 @@
 console.log('--step00');
 
 var fs = require('fs');
-var masters = fs.readFileSync('./master.txt', 'utf8').split('\n');
+var masters = JSON.parse(fs.readFileSync('./master.txt', 'utf8'));
 var listFiles = fs.readdirSync('./list', 'utf8');
 
+var zabbix = new Object();
+zabbix.data = new Array();
 // listFiles.map(function(x){console.log(x)});
 // master.map(function(x){console.log(x)});
+// console.log(typeof(masters));
 
 console.log('--step01');
 
-master = masters[0].substring(1).replace('\"','');
-console.log(master);
+// var baseCurrency = masters[0].substring(1).replace('\"','');
+// var marketCurrency = masters[0];
+// console.log('marketCurrency: ' + marketCurrency);
+// console.log('length: ' + marketCurrency.length);
 
-for (i in listFiles){
-  if (listFiles[i].toUpperCase().indexOf(master) == -1) {
-    // pairs = fs.readFileSync('./list/'+listFiles[i], 'utf8').split('\n');
-    console.log(listFiles[i]);
-  }
-}
+masters.forEach(function(marketCurrency, seq){
+  // console.log('--step01.1')
+  // console.log('marketCurrency: ' + marketCurrency);
+  var marketPairs = new Object();
+  marketPairs['{#SEQ}'] = seq;
+  marketPairs['{#NAME}'] = marketCurrency;
+
+  listFiles.forEach(function(listFile){
+    if (listFile.toUpperCase().indexOf(marketCurrency) == -1) {
+
+      marketPair = JSON.parse(fs.readFileSync('./list/'+listFile, 'utf8'));
+      // console.log((marketPair));
+      marketPair.forEach(function(x){
+        if (x.toUpperCase().indexOf(marketCurrency) != -1){
+          // console.log(listFile + ' ' + x);
+          marketPairs['{#' + listFile.replace('.txt', '}')] = x;
+        }
+      })
+    }
+  })
+  zabbix.data.push(marketPairs);
+});
+
+
 
 console.log('--step02');
 
-// fs.readdirSync('./list', 'utf8').map(function(x){console.log(x)});
-fs.readdirSync('./list', 'utf8').map(
-  function(x){
-    y = fs.readFileSync('./list/' + x, 'utf8');
-    read(y);
-  }
-);
-
+// console.log(zabbix);
 
 // fs.readFileSync('./list/btc_coincheck.txt', 'utf8').split('\n').map(function(x){console.log(x)});
 
 
-fs.writeFile("./test.txt", "test OK!", function (err) {
+fs.writeFile("./zabbixCryptoDiscovery.txt", JSON.stringify(zabbix), function (err) {
   if (err) {
       throw err;
   }
 });
 
-
-function read(x){
-  // x.split('\n').map(function(y){console.log(y)});
-}
+console.log('--step03');
