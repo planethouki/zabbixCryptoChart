92c91dc49e7bf2fe0a0b6c7898b4a3a029e256b0
planethouki <syn_tok@hotmail.co.jp>
2017-09-09 12:10:00 +0900

    cd bash_source

--- get-data.sh
+++ get-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 
 curl -Ss https://api.zaif.jp/api/1/currency_pairs/all > data/zaif_currencypairs.txt
 curl -sS https://bittrex.com/api/v1.1/public/getmarketsummaries > data/bittrex_getmarketsummaries.txt
