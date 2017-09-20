4d0ac0e9b83e321494db04775b551d73fb87ee77
planet★箒星 <syn_tok@hotmail.co.jp>
2017-09-20 14:03:58 +0900

    kraken

--- get-data-hourly.sh
+++ get-data-hourly.sh
@@ -4,7 +4,8 @@ script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 cd $script_dir
 
 curl -sS https://api.bitflyer.jp/v1/markets > data/bitflyer_markets.txt
-curl -Ss https://api.zaif.jp/api/1/currency_pairs/all > data/zaif_currencypairs.txt
+curl -sS https://api.zaif.jp/api/1/currency_pairs/all > data/zaif_currencypairs.txt
+curl -sS https://api.kraken.com/0/public/AssetPairs > data/kraken_assetpairs.txt
 
 OERID=`cat ./openexchangerates.txt`
 curl -sS https://openexchangerates.org/api/latest.json?app_id=$OERID > data/openexchangerates_latest.txt
